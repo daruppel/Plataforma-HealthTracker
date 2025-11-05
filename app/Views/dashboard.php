02963fea66c367b66dded52795543732fd6e2422
Micaela Haag <micaelahaag@gmail.com>
2025-11-05 11:52:05 -0300

    sacando el versionado del dashboard y del footer

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,7 +2,7 @@
   <div class="col-md-12">
     <div class="card card-primary">
       <div class="card-header">
-        <h3 class="card-title">Bienvenido a HealthTracker v2</h3>
+        <h3 class="card-title">Bienvenido a HealthTracker</h3>
       </div>
       <div class="card-body">
         Bienvenido 👋 <?= session()->get('user_name'); ?>
