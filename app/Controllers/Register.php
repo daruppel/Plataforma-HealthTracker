10e1f5536646d28dbcd30053d3f45f6bc1b5e246
denis ruppel <denisruppelvilla@gmail.com>
2025-11-09 14:31:15 -0300

    todo listo

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -64,7 +64,7 @@ class Register extends BaseController
         }
 
         // Si pasa la validación, guardar
-        $modelo = new UsuarioModel();
+        $modelo = new UserModel();
         
         // Mapeo de nombres del formulario -> nombres de la base
         $datos = [
