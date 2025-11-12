f9475cab64b44e7857b3234c1e2b5aa56be25a27
Micaela Haag <micaelahaag@gmail.com>
2025-11-12 08:49:57 -0300

    cambio en update

--- app/Views/admin/users/update.php
+++ app/Views/admin/users/update.php
@@ -3,13 +3,13 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="modalActualizacionUsuarioLabel">Crear nuevo usuario</h5>
+        <h5 class="modal-title" id="modalActualizacionUsuarioLabel">Actualizar usuario</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
-      <form id="formNuevoUsuario" action="<?= base_url('admin/usuarios/store'); ?>" method="post">
+      <form id="formNuevoUsuario" action="<?= base_url('admin/usuarios/update'); ?>" method="post">
         <div class="modal-body">
 
           <!-- 🧍‍♂️ Nombre y Apellido -->
