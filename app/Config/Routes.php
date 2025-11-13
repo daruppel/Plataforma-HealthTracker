3383ede81410aba43beff6c100a27ae66d95b62f
Micaela Haag <micaelahaag@gmail.com>
2025-11-13 13:20:00 -0300

    Seguridad para el grupo admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,8 +14,12 @@ $routes->get('/register', 'Register::index');
 $routes->post('/register/registrar', 'Register::registrar');
 $routes->get('/dashboard', 'Dashboard::index');
 $routes->post('/dashboard', 'Dashboard::index');
-$routes->get('/admin/users', 'Admin\Users::index');
-$routes->post('/admin/users/create', 'Admin\Users::create');
-$routes->get('/admin/users/create', 'Admin\Users::create');
 
+$routes->group('admin', ['filter' => 'auth:Administrador'], function($routes) {
 
+    $routes->get('users', 'Admin\Users::index');
+    $routes->get('users/create', 'Admin\Users::create');
+    $routes->post('users/create', 'Admin\Users::create');
+
+    $routes->get('medical-entities', 'Admin\MedicalEntities::index');
+});
