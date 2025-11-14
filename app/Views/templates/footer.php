b85f4f4903ddc89bbfee71cf916790a9e3b3d2df
Micaela Haag <micaelahaag@gmail.com>
2025-11-14 19:40:17 -0300

    toasts

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -69,6 +69,44 @@
     </div>
   </div>
 
+  <?php if (session()->get('success')): ?>
+  <script>
+  $(document).Toasts('create', {
+      class: 'bg-success',
+      title: 'Éxito',
+      body: '<?= session()->get("success") ?>',
+      autohide: true,
+      delay: 3000,
+      icon: 'fas fa-check'
+  });
+  </script>
+  <?php endif; ?>
+
+  <?php if (session()->get('error')): ?>
+  <script>
+  $(document).Toasts('create', {
+      class: 'bg-danger',
+      title: 'Error',
+      body: '<?= session()->get("error") ?>',
+      autohide: true,
+      delay: 4000,
+      icon: 'fas fa-exclamation-triangle'
+  });
+  </script>
+  <?php endif; ?>
+
+  <?php if (session()->get('errors')): ?>
+  <script>
+  $(document).Toasts('create', {
+      class: 'bg-warning',
+      title: 'Errores de validación',
+      body: `<?php foreach(session()->get('errors') as $e){ echo "- $e<br>"; } ?>`,
+      autohide: false,
+      icon: 'fas fa-bug'
+  });
+  </script>
+  <?php endif; ?>
+
 
 
 </body>
