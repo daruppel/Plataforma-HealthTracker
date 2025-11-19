07501f645f6e6ab4d3a9d0005a6491bd948f4503
daruppel <denisruppelvill@gmail.com>
2025-11-19 11:44:58 -0300

    el modal se vuelve a abrir cuando se detecta un error de validación de datos

--- app/Views/admin/medical_entities/index.php
+++ app/Views/admin/medical_entities/index.php
@@ -19,6 +19,17 @@
             <tr>
               <td><?= $e['nombre']; ?></td>
               <td><?= $e['descripcion']; ?></td>
+              <td>
+                <a href="#" class="btn btn-sm btn-warning btn-edit">
+                <i class="fas fa-pen"></i></a>
+                
+                <a href="#" class="btn btn-sm btn-danger btn-delete">
+                <i class="fas fa-trash"></i></a>
+
+                <a href="#" class="btn btn-sm btn-info btn-change-pass">
+                <i class="fas fa-key"></i></a>
+              <!-- <a href="#" class="btn btn-sm btn-info" data-toggle="modal" data-target="#modalChangePass"><i class="fas fa-key"></i></a> -->
+              </td>
             </tr>
             <?php endforeach; ?>
           </tbody>
@@ -27,4 +38,11 @@
     </div>
   </div>
   <?= view('admin/medical_entities/create'); ?>
+  <?php if (session()->get('errors')): ?>
+<script>
+    $(document).ready(function () {
+        $('#modalNuevaEntidad').modal('show');
+    });
+</script>
+<?php endif; ?>
 </section>
