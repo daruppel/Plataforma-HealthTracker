7f80b91f46ba9864848d710a45389eb11cf4505b
daruppel <denisruppelvill@gmail.com>
2026-04-01 09:08:05 -0300

    agregada las rutas de tipo de tarea y tareas de plan de cuidado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,12 @@ $routes->group('admin', ['filter' => 'auth:Administrador'], function($routes) {
     $routes->post('medical-diagnosis/create', 'Admin\MedicalDiagnosis::create');
     $routes->post('medical-diagnosis/delete', 'Admin\MedicalDiagnosis::delete');
     $routes->post('medical-diagnosis/update', 'Admin\MedicalDiagnosis::update');
+    //tipo de tarea
+    $routes->get('taskTypes', 'Admin\TaskType::index');
+    $routes->get('taskTypes/create', 'Admin\TaskType::create');
+    $routes->post('taskTypes/create', 'Admin\TaskType::create');
+    $routes->post('taskTypes/delete', 'Admin\TaskType::delete');
+    $routes->post('taskTypes/update', 'Admin\TaskType::update');
 
 });
 //medicos
@@ -48,6 +54,11 @@ $routes->group('medical_staff', ['filter' => 'auth:Personal de salud'], function
     $routes->post('diagnosis/create', 'Doctor\Diagnosis::create');
     $routes->post('diagnosis/delete', 'Doctor\Diagnosis::delete');
     $routes->post('diagnosis/update', 'Doctor\Diagnosis::update');
+    //tareas de plan de cuidado
+    $routes->get('care-plan-task', 'Doctor\CarePlanTask::index');
+    $routes->post('care-plan-task/create', 'Doctor\CarePlanTask::create');
+    $routes->post('care-plan-task/delete', 'Doctor\CarePlanTask::delete ');
+    $routes->post('care-plan-task/update', 'Doctor\CarePlanTask::update');
 
 });
 //compartido administradores y medicos
