92874c8b70ad5dbd3eab99dbed1305e82832fff3
Micaela Haag <micaelahaag@gmail.com>
2026-04-01 12:10:56 -0300

    creacion de la ventana de create.php

--- app/Views/doctor/diagnosis/create.php
+++ app/Views/doctor/diagnosis/create.php
@@ -1,1 +1,12 @@
-<php
+<section class="content">
+    <div class="container-fluid">
+    <div class="card card-primary">
+        <div class="card-header">
+        <h3 class="card-title">Carga de diagnóstico</h3>
+        </div>
+        <div class="card-body">
+        <p>Acá tiene que ir el formulario para crear un nuevo diagnóstico</p>
+            
+        </div>
+    </div>
+</div>
