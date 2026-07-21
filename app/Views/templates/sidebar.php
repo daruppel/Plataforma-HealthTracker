99c852b94ec1720f29301f2d8541d0de83fab0bb
denis ruppel <denisruppelvilla@gmail.com>
2026-07-21 20:13:13 -0300

    modificacion del sidebard para que esté activa la opcion seleccionada

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -21,7 +21,7 @@
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
         <li class="nav-item">
-          <a href="<?= base_url('/dashboard'); ?>" class="nav-link active">
+          <a href="<?= base_url('/dashboard'); ?>" class="nav-link <?= (url_is('dashboard*') || url_is('/')) ? 'active' : '' ?>">
             <i class="nav-icon fas fa-home"></i>
             <p>Inicio</p>
           </a>
@@ -29,19 +29,19 @@
         <!-- Menú para Administradores --> 
         <?php if ($rol === '1'): ?>
           <li class="nav-item">
-            <a href="<?= base_url('/admin/users'); ?>" class="nav-link">
+            <a href="<?= base_url('/admin/users'); ?>" class="nav-link <?= url_is('admin/users*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-user"></i>
               <p>Usuarios</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/admin/medical-entities'); ?>" class="nav-link">
+            <a href="<?= base_url('/admin/medical-entities'); ?>" class="nav-link <?= url_is('admin/medical-entities*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-hospital"></i>
               <p>Entidades médicas</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/admin/medical-diagnosis'); ?>" class="nav-link">
+            <a href="<?= base_url('/admin/medical-diagnosis'); ?>" class="nav-link <?= url_is('admin/medical-diagnosis*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-stethoscope"></i>
               <p>Tipos de diagnosticos</p>
             </a>
@@ -50,25 +50,25 @@
         <!-- Menú para Médicos -->
         <?php if ($rol === '2'): ?>
           <li class="nav-item">
-            <a href="<?= base_url('/medical_staff/patients') ?>" class="nav-link">
+            <a href="<?= base_url('/medical_staff/patients') ?>" class="nav-link <?= url_is('medical_staff/patients*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-users"></i>
               <p>Pacientes</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/medical_staff/diagnosis'); ?>" class="nav-link">
+            <a href="<?= base_url('/medical_staff/diagnosis'); ?>" class="nav-link <?= url_is('medical_staff/diagnosis*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-stethoscope"></i>
               <p>Diagnosticos</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/medical_staff/care-plan'); ?>" class="nav-link">
+            <a href="<?= base_url('/medical_staff/care-plan'); ?>" class="nav-link <?= url_is('medical_staff/care-plan*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-user-md"></i>
               <p>Planes de Cuidado</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/medical_staff/statistics'); ?>" class="nav-link">
+            <a href="<?= base_url('/medical_staff/statistics'); ?>" class="nav-link <?= url_is('medical_staff/statistics*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-chart-bar"></i>
               <p>Estadísticas</p>
             </a>
@@ -77,19 +77,19 @@
         <!-- Menú para Pacientes -->
         <?php if ($rol === '3'): ?>
            <li class="nav-item">
-            <a href="<?= base_url('/paciente/cumplimiento'); ?>" class="nav-link">
+            <a href="<?= base_url('/paciente/cumplimiento'); ?>" class="nav-link <?= url_is('paciente/cumplimiento*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-clipboard-check"></i>
               <p>Mi Plan de Cuidado</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/paciente/care-plan-history'); ?>" class="nav-link">
+            <a href="<?= base_url('/paciente/care-plan-history'); ?>" class="nav-link <?= url_is('paciente/care-plan-history*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-file-medical-alt"></i>
               <p>Historial de Diagnosticos</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('/paciente/documentacion'); ?>" class="nav-link">
+            <a href="<?= base_url('/paciente/documentacion'); ?>" class="nav-link <?= url_is('paciente/documentacion*') ? 'active' : '' ?>">
               <i class="nav-icon fas fa-folder-open"></i>
               <p>Documentación Medica</p>
             </a>
